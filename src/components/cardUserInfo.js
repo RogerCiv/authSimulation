0f07f171f62d19c4ee1552f105fde43ca6a29b5d
RogerCiv <rogeliocivantos@gmail.com>
2023-12-09 23:59:33 +0100

    update whit COBE

--- src/components/cardUserInfo.js
+++ src/components/cardUserInfo.js
@@ -1,23 +1,47 @@
+import createGlobe from "https://cdn.skypack.dev/cobe";
 
-
-export function cardUserInfo(container,userData){
-//console.log(userData);
-    container.innerHTML = `
-    <div class="card text-white bg-primary mb-3" style="max-width: 30rem;">
+export function cardUserInfo(container, userData) {
+  //console.log(userData);
+  container.innerHTML = `
+    <div class="card d-flex justify-content-center align-items-center text-center text-white bg-primary mb-3" style="max-width: 30rem;">
   <div class="card-header">Bienvenido ${userData.firstname} ${userData.lastname} </div>
   <div class="card-body">
     <h4 class="card-title text-warning">Tus datos:</h4>
     <p class="card-text">Email : ${userData.email}</p>
   </div>
-  <div id="map"></div>
+  <canvas
+  id="cobe"
+  style="width: 500px; height: 500px"
+></canvas>
 </div>
-    `
-    let map = L.map('map').setView([userData.address.geo.lat, userData.address.geo.lng], 13);
-    console.log(userData.address.geo);
-  
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
 
+    `;
+  let phi = 0;
+  let canvas = document.getElementById("cobe");
+
+  const globe = createGlobe(canvas, {
+    devicePixelRatio: 2,
+    width: 1000,
+    height: 1000,
+    phi: 0,
+    theta: 0,
+    dark: 0,
+    diffuse: 1.2,
+    scale: 1,
+    mapSamples: 16000,
+    mapBrightness: 6,
+    baseColor: [5, 5, 5],
+    markerColor: [1, 0.5, 1],
+    glowColor: [1, 1, 1],
+    offset: [0, 0],
+    markers: [
+      { location: [42.1234, -71.2345], size: 0.1 },
+    ],
+    onRender: (state) => {
+      // Called on every animation frame.
+      // `state` will be an empty object, return updated params.
+      state.phi = phi
+      phi += 0.01
+    },
+  })
 }
